a8a4a45b7441a9cf9342487ef437039c05b3d39d
Anis Najib <anis.najib@email.com>
2022-11-05 17:52:17 +0700

    Finish Up Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Experience from "./screens/Experience";
+import Portofolio from "./screens/Portofolio";
+import Contact from "./screens/Contact";
+
+import Headers from "./components/Header";
+import FloatinNavigator from "./components/FloatinNavigator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Headers />
+      <FloatinNavigator />
+      <Home />
+      <About />
+      <Experience />
+      <Experience />
+      <Portofolio />
+      <Contact />
+    </>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+const Headers = () => {
+  return <div>Headers</div>;
+};
+
+export default Headers;
