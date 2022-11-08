57b6ebf94f8c6574a292d69d9c138c217186a675
Anis Najib <anis.najib@email.com>
2022-11-08 11:32:05 +0700

    Home Intro turn into Responsive

--- src/screens/Home/component/HomeSocial.tsx
+++ src/screens/Home/component/HomeSocial.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
 
+import { FiMail } from "react-icons/fi";
+
 const HomeSocial = () => {
   return (
-    <div className='home__social'>
-      <a href='https://linkedin.com' target='_blank'>
-        <BsLinkedin />
-      </a>
-      <a href='https://github.com' target='_blank'>
-        <BsGithub />
-      </a>
-      <a href='https://instagram.com' target='_blank'>
-        <BsInstagram />
-      </a>
-    </div>
+    <>
+      <div className='home__social_left'>
+        <a href='https://linkedin.com' target='_blank'>
+          <BsLinkedin />
+        </a>
+        <a href='https://github.com' target='_blank'>
+          <BsGithub />
+        </a>
+      </div>
+      <div className='home__social_right'>
+        <a href='https://linkedin.com' target='_blank'>
+          <BsInstagram />
+        </a>
+        <a href='mailto:anis.najib@outlook.com' target='_blank'>
+          <FiMail />
+        </a>
+      </div>
+    </>
   );
 };
 
